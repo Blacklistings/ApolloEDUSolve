c8b3fed34089474b3e5f11e6ec91521accb628aa
Chiraag Chakravarthy <beaverduck99@gmail.com>
2020-08-14 16:53:16 -0400

    Added Start Page

--- app/src/main/java/com/example/apolloedusolve/MainActivity.java
+++ app/src/main/java/com/example/apolloedusolve/MainActivity.java
@@ -3,6 +3,8 @@ package com.example.apolloedusolve;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void employeeRegister(View view){
+
+    }
+
+    public void businessRegister(View view){
+
     }
 }
